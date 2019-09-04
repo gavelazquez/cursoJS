f51022acb7743487eb56961b968d0ec6d9978574
Christian Baus <christianbaus@hotmail.com>
2019-09-03 23:59:51 -0300

    Add files via upload

---
+++ 8-TPs/jsPiedarPapelTijera2.js
@@ -0,0 +1,31 @@
+var eleccionMaquina;
+var ContadorDeEmpates=0;
+var ContadorDeGanadas=0;
+var ContadorDePerdidas=0;
+
+function comenzar()
+{
+	//Genero el número RANDOM entre 1 y 3
+	 	
+
+
+
+}//FIN DE LA FUNCIÓN
+function piedra()
+{
+	
+
+}//FIN DE LA FUNCIÓN
+function papel()
+{
+
+}//FIN DE LA FUNCIÓN
+function tijera()
+{
+	
+}//FIN DE LA FUNCIÓN
+
+function mostrarResultado()
+{
+
+}
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# cursoJS
+# CursoIngresoJS
+Curso de ingreso con JavaScript
+
+ejercitación y TP para rendir el examen de ingreso 
+<h1>UTNFRA</h1>
+<h3>Tecnico superior en programación</h3>
+
+
+http://octaviovillegas.github.io/CursoIngresoJS/index.html
