a0c3388c97f8b06994564cd3333902a771d90263
gavelazquez <gavelazquez@users.noreply.github.com>
2019-09-11 16:36:33 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteraciones (1).js
+++ 5-InstruccionWhile/jsIteraciones (1).js
@@ -1,6 +1,17 @@
 function mostrar()
 {
-	alert('iteración while');
+	var i=0;
+	
+	while(i < 10){
+		i++; //suma 1
+		if(i % 2){ //cuando devuelve cero equivale a false
+			//muestro los pares
+			continue;
+		}
 
-
+		console.log("Cont: " + i);
+		//uso de break con if
+		//uso de continue
+	}
+	//console.log("Afuera Cont: " + i);
 }//FIN DE LA FUNCIÓN
