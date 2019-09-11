c5d19680fcfe1a19514f01d5131164d2a19cf4f1
gavelazquez <gavelazquez@users.noreply.github.com>
2019-09-11 16:00:50 -0300

    Delete jsEntradaSalida-2.js

--- jsEntradaSalida-2.js
+++
@@ -1,8 +0,0 @@
-/*Debemos lograr tomar un nombre con 'prompt' 
-y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
-function mostrar() {
-    var nombre;
-    nombre = prompt("Ingrese su nombre: ");
-    alert(nombre);
-}
-
