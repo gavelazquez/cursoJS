3ed898e40d2c5efe366b855d69870313c4be5cd2
gavelazquez <gavelazquez@users.noreply.github.com>
2019-09-19 12:04:30 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -1,6 +1,26 @@
 function mostrar()
 {
+	/*Al presionar el Boton, asignar una nota RANDOM al examen y mostrar:
+	"EXCELENTE" para notas igual a 9 o 10,
+	"APROBÓ" para notas mayores a 4,
+	"Vamos, la proxima se puede" para notas menores a 4 */
 	//Genero el número RANDOM entre 1 y 10 
-	
+	var max = 10;
+	var min = 1;
+	var num = Math.floor(Math.random()*((max+1)-min)+min);	
+	//Math.floor devuelve el valor entero menor
+
+	if(num >8){
+
+		alert('EXCELENTE');
+	}
+	else{
+		if (num < 4){
+			alert("Vamos, la proxima se puede");
+		}
+		else{
+			alert("APROBÓ");
+		}
+	}
 
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (8).js
+++ 2-InstruccionIf/jsInstruccionIF (8).js
@@ -1,7 +1,15 @@
 function mostrar()
 {
 //tomo la edad  
+/*Al ingresar una edad menor a 18 años y un estado civil distinto a "Soltero", NO HACER NADA,
+pero si no es asi, y es soltero y no es menor, mostrar el siguiente mensaje: 'Es soltero y no es menor.' */
+var edad = parseInt(document.getElementById("edad").value);
+var estado = document.getElementById("estadoCivil").value;
 
+if (!(edad < 18 && estado != "Soltero")) {
+    alert("Es soltero y no es menor.");
+
+}
 	
 
 
